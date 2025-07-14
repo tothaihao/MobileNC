ea12af6982e741a44fdffb732b5782d6c1520829
tothaihao <thaihaoto@gmail.com>
2025-07-14 14:40:32 +0700

    sửa config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -1,3 +1,3 @@
 class Config {
-  static const String baseUrl = 'http://10.21.12.24:5000'; // Đặt IP mặc định, có thể thay đổi
+  static const String baseUrl = 'https://mobilenc.onrender.com'; // Đặt IP mặc định, có thể thay đổi
 }
